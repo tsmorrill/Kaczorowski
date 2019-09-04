fd0ce858313ecab30dd73225b9d663fe66969b3d
Thomas Morrill <t.morrill@adfa.edu.au>
2019-09-05 09:44:58 +1000

    Update Rouche.py
    corrected middle_sum

--- Rouche.py
+++ Rouche.py
@@ -26,17 +26,10 @@ def init_sum(xi, epsilon):
 def middle_sum(xi, epsilon):
     """Calculate upper bound for sum_1^length a_i(z) on circle of radius epsilon about xi."""
     u, v = xi.real, xi.imag
-    k = fl(u/pi)
-    u2 = u - k*pi
-    if 0 <= u2 - epsilon <= u2 + epsilon <= pi/2:
-        sin_min = sin(u2 - epsilon)
-    elif pi/2 <= u2 - epsilon <= u2 + epsilon <= pi:
-        sin_min = sin(u2 + epsilon)
     sum = 0
-    for i in range(1, length + 1):
-        gamma = zeros[i-1]
+    for gamma in zeros[1:]:
         rho = 1/2 + gamma*1j
-        sum += exp(-gamma*exp(u - epsilon)*sin_min)/abs(rho)
+        sum += exp(-gamma*(v - epsilon))/abs(rho)
     return sum
 
 def tail_sum(xi, epsilon):
