86f72f41c595c7064580bf31f06f2935a7cc255b
Thomas Morrill <t.morrill@adfa.edu.au>
2019-09-05 09:59:47 +1000

    Update Rouche.py
    removed init_sum, middle_sum; added FN_min; renamed GN_max, Rouche_test

--- Rouche.py
+++ Rouche.py
@@ -6,6 +6,7 @@ from numpy import pi as pi
 from numpy import sin as sin
 
 from scipy.special import expn as E1 # -Ei(-x) = E1(x)
+from scipy.optimize import minimize
 
 length = 1000
 
@@ -16,29 +17,25 @@ zeros = [float(string) for string in zeros[:length]]
 file.close()
 print('Approximating by using first {} zeros of zeta.'.format(len(zeros)))
 
-def init_sum(xi, epsilon):
-    """Calculate lower bound for a_1(z) on circle of radius epsilon about xi."""
-    u, v = xi.real, xi.imag
-    gamma = 14.134725142
-    rho = 1/2 + gamma
-    return exp(-gamma*(v + epsilon))/abs(rho)
-
-def middle_sum(xi, epsilon):
-    """Calculate upper bound for sum_1^length a_i(z) on circle of radius epsilon about xi."""
-    u, v = xi.real, xi.imag
+def FN(z):
     sum = 0
-    for gamma in zeros[1:]:
-        rho = 1/2 + gamma*1j
-        sum += exp(-gamma*(v - epsilon))/abs(rho)
+    for zero in zeros:
+        sum += exp(complex(0, zero)*z)/complex(0.5, zero)
     return sum
 
-def tail_sum(xi, epsilon):
-    """Calculate upper bound for sum_{length+1} a_i(z) on circle of radius epsilon about xi."""
+def FN_min(xi, epsilon):
+    """Calculate lower point for FN on circle of radius epsilon about xi."""
+    def f(t):
+        return abs(FN(xi + epsilon*exp(t*1j)))
+    return minimize(f, 0).fun
+
+def GN_max(xi, epsilon):
+    """Calculate upper bound for GN on circle of radius epsilon about xi."""
     u, v = xi.real, xi.imag
     y = v - epsilon
     gamma = tail[0]
     Ei = -E1(1, y*gamma)
     return (1/y + 4*log(gamma*(1-1/2/pi)))*exp(-y*gamma) + 2*Ei/log(gamma/2/pi)
 
-def triple_play(xi, epsilon):
-    return(init_sum(xi, epsilon) - middle_sum(xi, epsilon), tail_sum(xi, epsilon))
+def Rouche_test(xi, epsilon):
+    return(FN_min(xi, epsilon), GN_max(xi, epsilon))
